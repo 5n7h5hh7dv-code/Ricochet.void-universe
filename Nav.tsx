173f2467caac21d99cf4ad6be45fdbd46d1dcac6
5n7h5hh7dv-code <5n7h5hh7dv@privaterelay.appleid.com>
2026-05-23 11:29:15 -0400

    Delete Nav.tsx

--- Nav.tsx
+++
@@ -1,2 +0,0 @@
-import Link from 'next/link';
-export function Nav(){return <nav className="nav"><Link href="/">1RicochetVoidUniverse</Link><span/><Link href="/archives">Foundations</Link><Link href="/submit">Submit</Link><Link href="/pricing">Access</Link><Link href="/future-gear">Future Gear</Link><Link href="/login">Login</Link></nav>}
