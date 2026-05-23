0ffac34e0264cfea33d9b287c0dacd67973a5d16
5n7h5hh7dv-code <5n7h5hh7dv@privaterelay.appleid.com>
2026-05-23 11:56:08 -0400

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,3 +0,0 @@
-import './globals.css';import {Nav} from '@/components/Nav';
-export const metadata={title:'1RicochetVoidUniverse',description:'Some doors are earned.'};
-export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="en"><body><Nav/>{children}<a className="far-door" href="/creator-vault">signal fracture</a></body></html>}
