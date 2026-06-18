87947f5041d99ce324bf6a89154fde1a87f0ab76
Oakley Cheuvront <5n7h5hh7dv@privaterelay.appleid.com>
2026-06-18 19:23:09 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 
+window.React = React;
+
 createRoot(document.getElementById("root")).render(<App />);
